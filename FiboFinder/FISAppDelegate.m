8ca57c650e98c57e14ff9769c6fdd94318138af2
John Richardson <richardsonjm@gmail.com>
2016-04-28 13:24:26 -0400

    Fibo found

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,22 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    for(NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        } else if (i == 1) {
+            [sequence addObject:@1];
+        } else {
+            NSInteger firstValue = [sequence[i - 2] integerValue];
+            NSInteger secondValue = [sequence[i-1] integerValue];
+            NSNumber *sum = @(firstValue + secondValue);
+            [sequence addObject:sum];
+        }
+    }
+    NSLog(@"%@", sequence);
+    return sequence;
+}
 
 @end
